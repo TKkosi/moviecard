2b3445faa257118c686811d8fcce138e88abe3a3
Ekwuno kosi <kosisochukwu13@yahoo.com>
2024-10-16 20:58:27 +0100

    filtered

--- vite-project/src/components/Filtered.jsx
+++ vite-project/src/components/Filtered.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
 
-const Filtered = () => {
+
+const Filtered = ({title , Movies}) => {
     return (
-        <div>filtered</div>
+        <div>
+            {
+                Movies.filter((movie) => movie.title.toLowerCase().includes(title.toLowerCase())).map((movie) => (
+                    <Movies key={movie.id} movie={movie} />
+                ))
+                }
+        </div>
     )
-}
+    }
 
 export default Filtered
